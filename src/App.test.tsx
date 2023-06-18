bc14ae44881c0c875e58a51279c35eac15c2f01a
Alex <alexhuo97@gmail.com>
2023-06-18 20:15:46 +1200

    initial

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,137 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setupInitialState = () => {
+    const utils = render(<App />);
+    return utils;
+};
+
+describe("App", () => {
+    let inputField: HTMLElement;
+    let submitButton: HTMLElement;
+    let reportButton: HTMLElement;
+    let rightButton: HTMLElement;
+    let leftButton: HTMLElement;
+    let positionElement: HTMLElement;
+    let errorMsg: HTMLElement;
+    let moveButton: HTMLElement;
+
+    beforeEach(() => {
+        setupInitialState();
+        inputField = screen.getByPlaceholderText("e.g. PLACE 1,2,EAST");
+        submitButton = screen.getByText("Submit");
+        reportButton = screen.getByText("Report");
+        rightButton = screen.getByTestId("rightBtn");
+        leftButton = screen.getByTestId("leftBtn");
+        moveButton = screen.getByText("Move");
+        positionElement = screen.getByTestId("position");
+        errorMsg = screen.getByTestId("errorMsg");
+    });
+
+    test("renders correctly", () => {
+        const gameText = screen.getByText("Pac-Man Game");
+        expect(gameText).toBeInTheDocument();
+    });
+
+    test("updates position and direction correctly when valid input is submitted", () => {
+        fireEvent.change(inputField, { target: { value: "PLACE 1,2,EAST" } });
+        fireEvent.click(submitButton);
+        fireEvent.click(reportButton);
+
+        expect(positionElement.textContent).toBe("Output: 1,2,EAST");
+    });
+
+    test("displays relevant error message for invalid inputs", () => {
+        fireEvent.change(inputField, { target: { value: "INVALID COMMAND" } });
+        fireEvent.click(submitButton);
+
+        expect(errorMsg.textContent).toBe("Input must start with 'PLACE ' ");
+
+        fireEvent.change(inputField, { target: { value: "PLACE 1,2" } });
+        fireEvent.click(submitButton);
+
+        expect(errorMsg.textContent).toBe("Incorrectly formatted input");
+
+        fireEvent.change(inputField, { target: { value: "PLACE 10,2,EAST" } });
+        fireEvent.click(submitButton);
+
+        expect(errorMsg.textContent).toBe("Coordinates out of bounds");
+    });
+
+    test("changes direction correctly when arrow icons are clicked", () => {
+        fireEvent.change(inputField, {
+            target: { value: "PLACE 0,0,NORTH" },
+        });
+        fireEvent.click(submitButton);
+
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,NORTH");
+
+        fireEvent.click(rightButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,EAST");
+
+        fireEvent.click(rightButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,SOUTH");
+
+        fireEvent.click(rightButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,WEST");
+
+        fireEvent.click(rightButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,NORTH");
+
+        fireEvent.click(leftButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,WEST");
+
+        fireEvent.click(leftButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,SOUTH");
+
+        fireEvent.click(leftButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,EAST");
+
+        fireEvent.click(leftButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 0,0,NORTH");
+    });
+
+    test("Pac-Man moves as expected 1 space north", () => {
+        fireEvent.change(inputField, { target: { value: "PLACE 0,0,NORTH" } });
+        fireEvent.click(submitButton);
+        fireEvent.click(moveButton);
+        fireEvent.click(reportButton);
+        expect(positionElement?.textContent).toBe("Output: 0,1,NORTH");
+    });
+
+    test("Pac-Man moves from 1,2,EAST to 3,3,NORTH", () => {
+        fireEvent.change(inputField, {
+            target: { value: "PLACE 1,2,EAST" },
+        });
+        fireEvent.click(submitButton);
+
+        fireEvent.click(moveButton);
+        fireEvent.click(moveButton);
+        fireEvent.click(leftButton);
+        fireEvent.click(moveButton);
+
+        fireEvent.click(reportButton);
+
+        expect(positionElement.textContent).toBe("Output: 3,3,NORTH");
+    });
+    test("Pac-Man attempts to move off grid", () => {
+        fireEvent.change(inputField, {
+            target: { value: "PLACE 4,0,NORTH" },
+        });
+        fireEvent.click(submitButton);
+        fireEvent.click(rightButton);
+        fireEvent.click(moveButton);
+        fireEvent.click(reportButton);
+        expect(positionElement.textContent).toBe("Output: 4,0,EAST");
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,193 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [position, setPosition] = useState<String>("");
+    const [direction, setDirection] = useState<string>("EAST");
+    const [gridItems, setGridItems] = useState<any[]>([]);
+    const [errorMsg, setErrorMsg] = useState("");
+    const [input, setInput] = useState("");
+    const [output, setOutput] = useState("");
+
+    const handleChangeDirection = (direction: String) => {
+        switch (direction) {
+            case "EAST":
+                return (
+                    <div
+                        data-testid="position"
+                        className="w-7 h-5 rounded-full border-t-[20px] border-b-[20px] border-l-[20px] border-r-[20px] md:border-t-[30px] md:border-b-[30px] md:border-l-[30px] md:border-r-[30px] border-l-yellow-400 border-t-yellow-400 border-b-yellow-400 border-r-transparent"
+                    />
+                );
+            case "WEST":
+                return (
+                    <div
+                        data-testid="position"
+                        className="w-7 h-5 rounded-full  border-t-[20px] border-b-[20px] border-l-[20px] border-r-[20px] md:border-t-[30px] md:border-b-[30px] md:border-l-[30px] md:border-r-[30px] border-r-yellow-400 border-t-yellow-400 border-b-yellow-400 border-l-transparent"
+                    />
+                );
+            case "NORTH":
+                return (
+                    <div
+                        data-testid="position"
+                        className="w-7 h-5 rounded-full border-t-[20px] border-b-[20px] border-l-[20px] border-r-[20px] md:border-t-[30px] md:border-b-[30px] md:border-l-[30px] md:border-r-[30px] border-l-yellow-400 border-r-yellow-400 border-b-yellow-400 border-t-transparent"
+                    />
+                );
+            case "SOUTH":
+                return (
+                    <div
+                        data-testid="position"
+                        className="w-7 h- rounded-full border-t-[20px] border-b-[20px] border-l-[20px] border-r-[20px] md:border-t-[30px] md:border-b-[30px] md:border-l-[30px] md:border-r-[30px] border-l-yellow-400 border-t-yellow-400 border-r-yellow-400 border-b-transparent"
+                    />
+                );
+        }
+    };
+
+    const handleMove = useCallback(() => {
+        let col = Number(position.split(",")[0]);
+        let row = Number(position.split(",")[1]);
+        switch (direction) {
+            case "NORTH":
+                row = row < 4 ? row + 1 : row;
+                break;
+            case "SOUTH":
+                row = row > 0 ? row - 1 : row;
+                break;
+            case "WEST":
+                col = col > 0 ? col - 1 : col;
+                break;
+            case "EAST":
+                col = col < 4 ? col + 1 : col;
+                break;
+        }
+        setPosition(`${col},${row}`);
+    }, [direction, position]);
+
+    // Generate grid items
+    const renderGridItems = useCallback(() => {
+        let rows = 5;
+        let columns = 5;
+        let gridItems = [];
+        for (let row = rows - 1; row >= 0; row--) {
+            for (let col = 0; col < columns; col++) {
+                const key = `${col},${row}`;
+                gridItems.push(
+                    <div
+                        key={key}
+                        className="2xl:p-6 md:p-4 p-2 items-center border border-black">
+                        {position === key && handleChangeDirection(direction)}
+                    </div>
+                );
+            }
+        }
+        setGridItems(gridItems);
+    }, [position, direction]);
+
+    useEffect(() => {
+        renderGridItems();
+    }, [position, direction, renderGridItems]);
+
+    const handleSubmit = useCallback(() => {
+        if (!input.startsWith("PLACE ")) {
+            setErrorMsg("Input must start with 'PLACE ' ");
+            return;
+        }
+        if (input.split(" ").length !== 2) {
+            setErrorMsg("Incorrect amount of spaces");
+            return;
+        }
+        let command = input.split(" ")[1];
+        if (command.split(",").length !== 3) {
+            setErrorMsg("Incorrectly formatted input");
+            return;
+        }
+        let col = Number(command.split(",")[0]);
+        let row = Number(command.split(",")[1]);
+        let direction = command.split(",")[2];
+        if (col > 4 || col < 0 || row > 4 || row < 0) {
+            setErrorMsg("Coordinates out of bounds");
+            return;
+        }
+        setPosition(`${col},${row}`);
+        setDirection(direction);
+    }, [input]);
+
+    const handleRotate = (clockwise: boolean) => {
+        const directions = ["NORTH", "EAST", "SOUTH", "WEST"];
+        const currentIndex = directions.indexOf(direction);
+        const rotationOffset = clockwise ? 1 : -1;
+        const nextIndex =
+            (currentIndex + rotationOffset + directions.length) %
+            directions.length;
+        setDirection(directions[nextIndex]);
+    };
+
+    const reportCurrentPosition = useCallback(() => {
+        let outputString = `${position},${direction}`;
+        setOutput(outputString);
+    }, [position, direction]);
+
+    return (
+        <div className="App">
+            <div className="text-xl font-bold text-blue-400 pt-5">
+                Pac-Man Game
+            </div>
+            <div className="flex flex-col h-screen items-center">
+                <div className="lg:w-1/3 md:w-2/3 w-5/6 min-w-[300px] h-2/5 sm:h-2/3 bg-gray-200 border-2 grid grid-rows-5 grid-cols-5 my-5">
+                    {gridItems}
+                </div>
+                <div className="flex flex-row justify-start sm:w-1/3 gap-x-3 sm:gap-x-5 items-center">
+                    <div
+                        className="py-2 px-4 bg-blue-200 rounded-2xl cursor-pointer my-3"
+                        onClick={reportCurrentPosition}>
+                        Report
+                    </div>
+                    <div
+                        className="py-2 px-4 bg-blue-200 rounded-2xl cursor-pointer my-3"
+                        onClick={handleMove}>
+                        Move
+                    </div>
+                    <div data-testid="position">{`Output: ${output}`}</div>
+                </div>
+                <div className="flex flex-col sm:flex-row py-2 gap-x-10 items-center">
+                    <div className="flex flex-row gap-x-8 pb-5">
+                        <ArrowLeftIcon
+                            data-testid="leftBtn"
+                            className="w-10 h-10 text-black cursor-pointer"
+                            onClick={() => handleRotate(false)}
+                        />
+                        <ArrowRightIcon
+                            data-testid="rightBtn"
+                            className="w-10 h-10 text-black cursor-pointer"
+                            onClick={() => handleRotate(true)}
+                        />
+                    </div>
+                    <input
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                handleSubmit();
+                            }
+                        }}
+                        value={input}
+                        onChange={(e) => {
+                            setErrorMsg("");
+                            setInput(e.target.value.toUpperCase());
+                        }}
+                        placeholder="e.g. PLACE 1,2,EAST"
+                        className="border rounded-xl border-black px-4 h-10"
+                    />
+                    <div
+                        className="py-2 px-4 bg-blue-200 rounded-2xl cursor-pointer my-3"
+                        onClick={handleSubmit}>
+                        Submit
+                    </div>
+                </div>
+                <div data-testid="errorMsg" className="text-red-500">
+                    {errorMsg}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
